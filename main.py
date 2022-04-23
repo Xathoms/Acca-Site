cb5576718744e74200640b23cf822803c2ed1017
Xathoms <thomas.mortaro@jfrogmedia.com>
2022-04-23 14:05:42 +0200

    first template

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 from sqlalchemy.orm import Session
 import crud, schemas
 from DBConfig import SessionLocal
 import uvicorn
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
 
 # Dependency
 def get_db():
@@ -13,7 +17,11 @@ def get_db():
         yield db
     finally:
         db.close()
-         
+
+@app.get("/", response_model=schemas.User)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     # db_user = crud.get_user_by_email(db, email=user.email)
@@ -21,11 +29,14 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     #     raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
 
-@app.get("/users/{user_id}", response_model=schemas.User)
+
+@app.get("/users/{chat_id}", response_model=schemas.User)
 def read_user(chat_id: int, db: Session = Depends(get_db)):
     this_date = crud.get_this_date(db)
     last_date = crud.get_last_date(db)
-    db_user = crud.get_user(db, chat_id=chat_id, this_date=this_date.Date, last_date=last_date.Date)
+    db_user = crud.get_user(
+        db, chat_id=chat_id, this_date=this_date.Date, last_date=last_date.Date
+    )
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
